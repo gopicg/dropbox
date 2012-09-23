6ccbc3eef33f0f742d0eff9a9a62fbf3f5261ea4
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2012-09-23 18:07:49 -0400

    Authentication is moving out from Storage

--- src/main/java/org/syncloud/dropbox/DropboxStorageMetadata.java
+++ src/main/java/org/syncloud/dropbox/DropboxStorageMetadata.java
@@ -1,8 +1,6 @@
 package org.syncloud.dropbox;
 
-import syncloud.storage.AuthenticationType;
-import syncloud.storage.IStorage;
-import syncloud.storage.IStorageMetadata;
+import syncloud.storage.*;
 
 import java.util.EnumSet;
 
@@ -20,5 +18,15 @@ public class DropboxStorageMetadata extends IStorageMetadata {
     @Override
     public EnumSet<AuthenticationType> getAuthenticationTypes() {
         return EnumSet.of(AuthenticationType.OAuth);
+    }
+
+    @Override
+    public IAuthentication getAuthentication(AuthenticationType type) throws StorageException {
+        if (type == AuthenticationType.OAuth)
+            return new DropboxAuthentication();
+        else {
+            String message = IStorageMetadata.MessageUnsupportedAuthenticationType(type);
+            throw new StorageException(message);
+        }
     }
 }
